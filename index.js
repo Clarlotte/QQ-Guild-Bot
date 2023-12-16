0f9d8733f2cf9f8b6c4261290f7e868e1ea367d4
lzy <101228920+Clarlotte@users.noreply.github.com>
2023-12-16 22:19:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import { Bot, mountPlugin } from '@kokkoro/core'
  * @type {import('@kokkoro/core').BotConfig}
  */
 const config = {
-    appid: '102057740',
-    token: 'kO32sbWGyrCEmL93cjOtioQ1Q7Y4X6Ms',
-    secret: 'kOp21tY1FE7mCNJD',
+    appid: '##',
+    token: '##',
+    secret: '##',
     events: ['PUBLIC_GUILD_MESSAGES'],
     log_level: 'INFO' && 'DEBUG'
+    /** 日志等级，具体使用可查阅 log4js 文档 */
+    /**type LogLevel = 'OFF' | 'FATAL' | 'ERROR' | 'WARN' | 'INFO' | 'DEBUG' | 'TRACE' | 'ALL';*/
 }
 export const bot = new Bot(config)
