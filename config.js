8cd00e083a154094ef3cc4c3ea8456fd815583e0
Clarlotte <clarlotte007@gmail.com>
2024-02-05 15:45:11 +0000

    update config.js.
    Signed-off-by: Clarlotte <clarlotte007@gmail.com>

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import { Bot } from '@kokkoro/core'
  * @type {import('@kokkoro/core').BotConfig}
  */
 const config = {
-    appid: '102057740',
-    token: 'kO32sbWGyrCEmL93cjOtioQ1Q7Y4X6Ms',
-    secret: 'kOp21tY1FE7mCNJD',
+    appid: 'xxx',
+    token: 'xxxx',
+    secret: 'xxx',
     events: ['PUBLIC_GUILD_MESSAGES'],
     log_level: 'INFO' && 'DEBUG'
 }
